1705c35b5fb48a553fe2f6fc63c092daf15f139d
junsung <forest2847@gmail.com>
2022-11-24 11:30:01 +0900

    인트로씬 난이도 선택 캔버스

---
+++ Assets/01.Scripts/JunSung/SelectDifficulty.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectDifficulty : MonoBehaviour
+{
+    public GameObject selectCanvas;
+
+    public void ActiveSelectCanvas()
+    {
+        selectCanvas.SetActive(true);
+    }
+}
